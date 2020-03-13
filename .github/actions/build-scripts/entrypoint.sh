d66728d7d39aee8ee5eb9ca625c05885b1ecfac5
Emil Broman <emil@emilbroman.me>
2020-03-13 16:46:31 +0100

    Prep for CD

--- .github/actions/build-scripts/entrypoint.sh
+++ .github/actions/build-scripts/entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -- -y
 rustup default nightly
 yarn webpack --mode production
