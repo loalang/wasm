5a294c18f269349b7053b1fe50a90a1593c04772
Emil Broman <emil@emilbroman.me>
2020-03-13 17:03:53 +0100

    Prep for CD

--- .github/actions/wasm-pack/entrypoint.sh
+++ .github/actions/wasm-pack/entrypoint.sh
@@ -2,9 +2,4 @@
 
 set -e
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh /dev/stdin -y
-rustup default nightly
-
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-
 bash -c "$1"
